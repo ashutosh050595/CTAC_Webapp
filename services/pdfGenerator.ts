c23bd2ef9d65e290f3370268f772194bd9f4aa7b
ashutosh050595 <gautam663@gmail.com>
2026-01-21 10:29:19 +0530

    Update email address in PDF generator

--- services/pdfGenerator.ts
+++ services/pdfGenerator.ts
@@ -128,7 +128,7 @@ export const generatePDF = (data: ApplicationData, shouldDownload: boolean = tru
   centerText("(Affiliated to M.D., University, Rohtak)", 22, 10);
   doc.setFontSize(9);
   doc.text("Website : www.cracollegesonepat.org", 14, 28);
-  doc.text("email : trgcrecruitment2025@gmail.com", pageWidth - 14, 28, { align: 'right' });
+  doc.text("email : crarecruitment2026@gmail.com", pageWidth - 14, 28, { align: 'right' });
   doc.setLineWidth(0.5);
   doc.line(10, 30, pageWidth - 10, 30);
   
